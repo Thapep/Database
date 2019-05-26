633b8cbd55cb60b0ebc29e617f5fa81ca9e519f5
arkountos <eternallistener@gmail.com>
2019-05-26 03:31:30 +0300

    Added delete functionality through UI for Book table

--- delete_Book.php
+++ delete_Book.php
@@ -18,23 +18,42 @@ $type = $_POST['type'];
 //Input parameter we are looking to delete
 $input = $_POST['input'];
 
+$sql_delete = '';
 
-if ($type == 'ISBN'){
-    $sql = 'DELETE FROM Book WHERE ISBN = $input;'
-}
-else if ($type == 'title'){
-    $sql = 'DELETE FROM Book WHERE title = $input;'
-}
-else if ($type == 'pubYear'){
-    $sql = 'DELETE FROM Book WHERE pubYear = $input;'
-}
-else if ($type == 'numpages'){
-    $sql = 'DELETE FROM Book WHERE numpages = $input;'
-}
-else if ($type == 'pubName'){
-    $sql = 'DELETE FROM Book WHERE pubName = $input;'
+$ISBN = $_POST['ISBN'];
+
+// If instead of the empty string ('') I use NULL the comparison returns TRUE (damn php and your types)
+if ($ISBN !== ''){
+    $sql_delete = $sql_delete . 'ISBN = ' . $ISBN . ' and ';
 }
 
+// Pay attention to the escaping quotes. That's because we want to turn the input into a string!
+$title = $_POST['title'];
+if ($title !== ''){
+    $sql_delete = $sql_delete . 'title = \'' . $title . '\' and ';
+}
+$pubYear = $_POST['pubYear'];
+if ($pubYear !== ''){
+    $sql_delete = $sql_delete . 'pubYear = ' . $pubYear . ' and ';
+}
+$numpages = $_POST['numpages'];
+if ($numpages !== ''){
+    $sql_delete = $sql_delete . 'numpages = ' . $numpages . ' and ';
+}
+$pubName = $_POST['pubName'];
+if ($pubName !== ''){
+    $sql_delete = $sql_delete . 'pubName = \'' . $pubName . '\' and ';
+}
+if ($sql_delete == ''){
+    echo 'You have to fill at least one field to delete!';
+    die();
+}
+$sql_delete = substr($sql_delete, 0, -5);
+
+$sql = '
+DELETE FROM Book
+WHERE ' . $sql_delete . ';
+';
 
 if(!$result = $db->query($sql)){
     die('There was an error running the query [' . $db->error . ']');
