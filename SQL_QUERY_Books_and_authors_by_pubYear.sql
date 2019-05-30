e1dfa83c655b8eb4b1bee7e6f5aa0cc0bb5598b2
arkountos <eternallistener@gmail.com>
2019-05-30 20:01:31 +0300

    Displays all books and their authors ordered by pubYear

---
+++ SQL_QUERY_Books_and_authors_by_pubYear.sql
@@ -0,0 +1,5 @@
+SELECT written_by.ISBN, Book.title, author.AFirst, author.ALast
+FROM written_by LEFT JOIN Book
+ON Book.ISBN=written_by.ISBN
+LEFT JOIN author ON author.authID=written_by.authID
+ORDER BY pubYear ASC
