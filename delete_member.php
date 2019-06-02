1295114a197691597ad0ecb526e4a672c133ff92
root <konkou97@hotmail.com>
2019-06-02 23:09:13 +0300

    PHP for author-member delete and update

---
+++ delete_member.php
@@ -0,0 +1,65 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "SelectedAgainst";
+$dbname = "mydb";
+
+
+$db = new mysqli($servername, $username, $password, $dbname);
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+echo 'Succesfull Connection' . '<br />';
+
+//Choose how you will delete elements from the database (e.g. type = 'ISBN', so you delete based on ISBN)
+$type = $_POST['type'];
+//Input parameter we are looking to delete
+$input = $_POST['input'];
+
+$sql_delete = '';
+
+$memberID = $_POST['memberID'];
+
+// If instead of the empty string ('') I use NULL the comparison returns TRUE (damn php and your types)
+if ($memberID !== ''){
+    $sql_delete = $sql_delete . 'memberID = ' . $memberID . ' and ';
+}
+
+// Pay attention to the escaping quotes. That's because we want to turn the input into a string!
+$MFirst = $_POST['MFirst'];
+if ($MFirst !== ''){
+    $sql_delete = $sql_delete . 'MFirst = \'' . $MFirst . '\' and ';
+}
+$MLast = $_POST['MLast'];
+if ($MLast !== ''){
+    $sql_delete = $sql_delete . 'MLast = \'' . $MLast . '\' and ';
+}
+$Street = $_POST['Street'];
+if ($Street !== ''){
+    $sql_delete = $sql_delete . 'Street = \'' . $Street . '\' and ';
+}
+$st_number = $_POST['st_number'];
+if ($st_number !== ''){
+    $sql_delete = $sql_delete . 'st_number = ' . $st_number . ' and ';
+}
+$postalCode = $_POST['postalCode'];
+if ($postalCode !== ''){
+    $sql_delete = $sql_delete . 'postalCode = ' . $postalCode . ' and ';
+}
+if ($sql_delete == ''){
+    echo 'You have to fill at least one field to delete!';
+    die();
+}
+$sql_delete = substr($sql_delete, 0, -5);
+
+$sql = '
+DELETE FROM member
+WHERE ' . $sql_delete . ';
+';
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+?>
---
+++ update_member.php
@@ -0,0 +1,96 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "SelectedAgainst";
+$dbname = "mydb";
+
+$db = new mysqli($servername, $username, $password, $dbname);
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+echo 'Succesfull Connection' . '<br />';
+
+$sql_search = '';
+$sql_replace = '';
+
+$memberID = $_POST['memberID'];
+
+// If instead of the empty string ('') I use NULL the comparison returns TRUE (damn php and your types)
+if ($memberID !== ''){
+    $sql_search = $sql_search . 'memberID = ' . $memberID . ' and ';
+}
+
+// Pay attention to the escaping quotes. That's because we want to turn the input into a string!
+$MFirst = $_POST['MFirst'];
+if ($MFirst !== ''){
+    $sql_search = $sql_search . 'MFirst = \'' . $MFirst . '\' and ';
+}
+$MLast = $_POST['MLast'];
+if ($MLast !== ''){
+    $sql_search = $sql_search . 'MLast = \'' . $MLast . '\' and ';
+}
+$Street = $_POST['Street'];
+if ($Street !== ''){
+    $sql_search = $sql_search . 'Street = \'' . $Street . '\' and ';
+}
+$st_number = $_POST['st_number'];
+if ($st_number !== ''){
+    $sql_search = $sql_search . 'st_number = ' . $st_number . ' and ';
+}
+$postalCode = $_POST['postalCode'];
+if ($postalCode !== ''){
+    $sql_search = $sql_search . 'postalCode = ' . $postalCode . ' and ';
+}
+if ($sql_search == ''){
+    echo 'You have to fill at least one field to search for!';
+    die();
+}
+$sql_search = substr($sql_search, 0, -5);
+
+$memberID_new = $_POST['memberID_new'];
+if ($memberID_new !== ''){
+    $sql_replace = $sql_replace . 'memberID = ' . $memberID_new . ' and ';
+}
+$MFirst_new = $_POST['MFirst_new'];
+if ($MFirst_new !== ''){
+    $sql_replace = $sql_replace . 'MFirst = \'' . $MFirst_new . '\' and ';
+}
+$MLast_new = $_POST['MLast_new'];
+if ($MLast_new !== ''){
+    $sql_replace = $sql_replace . 'MLast = \'' . $MLast_new . '\' and ';
+}
+$Street_new = $_POST['Street_new'];
+if ($Street_new !== ''){
+    $sql_replace = $sql_replace . 'Street = \'' . $Street_new . '\' and ';
+}
+$st_number_new = $_POST['st_number_new'];
+if ($st_number_new !== ''){
+    $sql_replace = $sql_replace . 'st_number = ' . $st_number_new . ' and ';
+}
+$postalCode_new = $_POST['postalCode_new'];
+if ($postalCode_new !== ''){
+    $sql_replace = $sql_replace . 'postalCode = ' . $postalCode_new . ' and ';
+}
+
+if ($sql_replace == ''){
+    echo 'You have to fill at least one field to change!';
+    die();
+}
+$sql_replace = substr($sql_replace, 0, -5);
+
+
+echo $sql_search . '<br />' . $sql_replace . '<br />';
+
+$sql = '
+UPDATE member
+SET ' . $sql_replace . '
+WHERE ' . $sql_search . ';
+';
+
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+?>
