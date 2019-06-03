c33ea41b88b1fe9bcac22b1cd4a089572d152b96
arkountos <eternallistener@gmail.com>
2019-06-03 20:20:20 +0300

    Added books by supercategory query functionality

---
+++ Books_by_supercategory.php
@@ -0,0 +1,51 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "SelectedAgainst";
+$dbname = "mydb";
+
+echo 'Hello World!' . '<br />';
+
+$db = new mysqli($servername, $username, $password, $dbname);
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+echo 'Succesfull Connection' . '<br />';
+
+$user_input =  $_POST['user_input'];
+
+//DANGER!!! INJECTION ATTACK VULNERABILITY
+if (isset($_POST['user_choice'])){
+    $sql = '
+SELECT supercategoryName as \'Supercategory\', COUNT(book_belongs.title) as \'Quantity\' FROM (
+	SELECT title, categoryName FROM Book JOIN belongs_to ON Book.ISBN = belongs_to.ISBN
+    ) as book_belongs JOIN category 
+    ON book_belongs.categoryName = category.categoryName
+GROUP BY supercategoryName
+ORDER BY COUNT(book_belongs.title) DESC
+';
+}
+
+else{
+    $sql = '
+SELECT supercategoryName as \'Supercategory\', COUNT(book_belongs.title) as \'Quantity\' FROM (
+	SELECT title, categoryName FROM Book JOIN belongs_to ON Book.ISBN = belongs_to.ISBN
+    ) as book_belongs JOIN category 
+    ON book_belongs.categoryName = category.categoryName
+GROUP BY supercategoryName
+HAVING supercategoryName = \'' . $user_input . ' \'
+ORDER BY COUNT(book_belongs.title) DESC
+';
+}
+
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+echo 'All good!<br />';
+
+while($row = $result->fetch_assoc()){
+    echo $row['Supercategory'] . ' ' . $row['Quantity'] . '<br />';
+}
