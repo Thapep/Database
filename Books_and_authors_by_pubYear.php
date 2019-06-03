703152c2d6d99ecdab599d586a009ed063310007
arkountos <eternallistener@gmail.com>
2019-06-03 17:18:28 +0300

    Yet another Minor fix

--- Books_and_authors_by_pubYear.php
+++ Books_and_authors_by_pubYear.php
@@ -29,5 +29,5 @@ if(!$result = $db->query($sql)){
 echo 'All good!<br />';
 
 while($row = $result->fetch_assoc()){
-    echo $row['title'] . ' ' . $row['ISBN'] . ' ' . $row['AFirst'] . ' ' . $row['ALast'] . '<br />';
+    echo $row['pubYear'] . ' ' . $row['title'] . ' ' . $row['ISBN'] . ' ' . $row['AFirst'] . ' ' . $row['ALast'] . '<br />';
 }
